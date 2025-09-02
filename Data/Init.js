434c84c64aece28bcdb1cee39a8f36b0667dc58f
MinionsArt <joyceplokker@hotmail.com>
2025-09-02 11:32:15 +0200

    Update Init.js

--- Data/Init.js
+++ Data/Init.js
@@ -2,22 +2,24 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     
-    
+    requireAjax("/aow4db/Data/src/settings.js", function () {
+         requireAjax("/aow4db/Data/Search.js", function () {
+              requireAjax("/aow4db/Data/Faction.js", function () {
+          
+      });
+          
+      });
+          
+      });
+      
+       
   
   // Load header
   fetch("/aow4db/HTML/header.html")
     .then((res) => res.text())
     .then((html) => {
        document.body.insertAdjacentHTML("afterbegin", html);
-      requireAjax("/aow4db/Data/src/settings.js", function () {
-          
-      });
-       requireAjax("/aow4db/Data/Search.js", function () {
-          
-      });
-        requireAjax("/aow4db/Data/Faction.js", function () {
-          
-      });
+      
       
       requireAjax("/aow4db/Data/Builder.js", function () {
    
@@ -33,6 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
  function requireAjax(file, callback) {
+     
+   
+     
   jQuery.getScript(file, callback);
 }
 
